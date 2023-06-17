0f508759ab26d9b96953ed0d76baa69258f62d0c
UmVitorAleatorio <vitorhiper@hotmail.com>
2023-06-17 01:22:30 +0000

    Atualizacao da pag Home e Sobre

--- pages/Sobre.js
+++ pages/Sobre.js
@@ -1,20 +1,14 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function Home({ navigation }) {
 	
   return (
     <View style={styles.container}>
-      <Text>Home Page</Text>
+      <Text>Sobre</Text>
+
 		  
-		  <TouchableOpacity
-		  	 onPress={() => navigation.navigate('Sobre')}
-			  >
-			  <text>Sobre</text>
-		  </TouchableOpacity>
-		  
+		  <text>Sobre a empresa</text>
     </View>
   );
 }
